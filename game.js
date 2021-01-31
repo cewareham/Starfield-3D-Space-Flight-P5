776809b03e74f4e40f85029ee63eea02282c9a8a
cewareham <cwareham007@gmail.com>
2021-01-30 21:56:01 -0700

    04 opening scene with  music

--- game.js
+++ game.js
@@ -4,6 +4,8 @@
 class Game {
 	constructor() {
       this.starfield = new StarField();
+      this.halfWidth = width*0.5;
+      this.halfHeight = height*0.5;
    }
 
    update = () => {
@@ -13,5 +15,7 @@ class Game {
    render = () => {
       background(0);
       this.starfield.render();
+      //translate(-width*0.5, -height*0.5);
+      image(spaceship, -this.halfWidth, -this.halfHeight, width, height, 0, 0);
    }
 }
--- sketch.js
+++ sketch.js
@@ -1,6 +1,15 @@
 "use strict";
 
 let game;
+let thumbnail, spaceship;
+let themeSong, laserSound;
+
+function preload() {
+  thumbnail = loadImage('assets/thumbnail.png');
+  spaceship = loadImage('assets/spaceship.png')
+  themeSong = new p5.SoundFile('assets/star wars.mp3', themeSongLoaded);
+  laserSound = new p5.SoundFile('assets/laser.wav');
+}
 
 function setup() {
   createCanvas(700, 700);
@@ -10,4 +19,8 @@ function setup() {
 function draw() {
   game.update();
   game.render();
+}
+
+function themeSongLoaded() {
+  themeSong.loop(); // loop()->play continuously, play()->plays once & quits
 }
